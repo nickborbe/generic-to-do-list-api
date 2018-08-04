1a2a2d9597d2895f256997c877384d4e9de79035
nickborbe <nickborbe@gmail.com>
2018-08-04 16:45:51 -0400

    full CRUD

--- routes/tasks.js
+++ routes/tasks.js
@@ -27,17 +27,29 @@ router.post('/tasks/create', (req, res, next)=>{
     })
 })
 
+router.post('/tasks/edit/:id', (req, res, next)=>{
+    Task.findByIdAndUpdate(req.params.id, req.body)
+    .then((response)=>{
+        res.json(response)
+    })
+    .catch((err)=>{
+        res.json(err)
+    })
+})
 
-/// task details route here 
+router.post('/tasks/delete/:id', (req, res, next)=>{
+    Task.findByIdAndRemove(req.params.id)
+    .then((response)=>{
+        res.json(response)
+    })
+    .catch((err)=>{
+        res.json(err)
+    })
+
+})
 
 
 
-
-/// edit task route here
-
-
-
-// delete task route
 
 
 
